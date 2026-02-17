5476125da670919713d56e0791880d72081f1dd6
Dallas Strouse <93224879+orowith2os@users.noreply.github.com>
2026-02-17 12:07:33 -0600

    Add toolbox installation to build script

--- build_files/build.sh
+++ build_files/build.sh
@@ -12,6 +12,8 @@ set -ouex pipefail
 # this installs a package from fedora repos
 dnf5 install -y tmux 
 
+dnf5 install -y toolbox
+
 # Use a COPR Example:
 #
 # dnf5 -y copr enable ublue-os/staging
